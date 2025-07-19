1892c26d88528aaf48cfc894a30c42668366fec6
Keiran <keircn@proton.me>
2025-07-19 08:49:46 +0100

    oops

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -114,7 +114,6 @@ end
 if status is-interactive
     set ZELLIJ_AUTO_ATTACH true
     set ZELLIJ_AUTO_EXIT true
-    eval (zellij setup --generate-auto-start fish | string collect)
 end
 
 if not set -q ZELLIJ
