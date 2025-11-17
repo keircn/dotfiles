7f223b990acebca6fa48949c23d0617bb504cf71
Keiran <keircn@proton.me>
2025-11-17 10:15:36 +0000

    add dependencies to setup script

--- setup.go
+++ setup.go
@@ -81,7 +81,7 @@ func main() {
 		os.Exit(0)
 	}
 
-	checkDependencies([]string{"stow", "git", "jq", "hyprland", "fish", "vim", "bash", "fastfetch", "ghostty", "mako", "waybar", "hypridle", "hyprlock", "hyprshot", "starship"})
+	checkDependencies([]string{"stow", "git", "jq", "hyprland", "fish", "vim", "bash", "fastfetch", "ghostty", "mako", "waybar", "hypridle", "hyprlock", "hyprshot", "starship", "hyprpicker", "hyprshell", "mpv"})
 
 	if listOnly {
 		apps := getAllApps(".")
