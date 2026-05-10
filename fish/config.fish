b2d5c49292f9761ffffb8ecfe3ba09c3fb556c88
Key <root@roxylab.uk>
2026-05-10 11:48:14 +0100

    ghostty

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,9 @@
 set -U fish_greeting
 set EDITOR hx
 set -gx GPG_TTY (tty)
+set QT_QPA_PLATFORMTHEME qt5ct
+set QT_AUTO_SCREEN_SCALE_FACTOR 1
+alias n nvim
 
 if status is-interactive
     starship init fish | source
